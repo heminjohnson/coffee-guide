5fc55554ea17dc7d5ea1f00034d7303e73836d02
Hemin Johnson <hemin.johnson@diconium.com>
2023-03-20 10:56:45 +0100

    feat: add health endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealth() {
+    const version = '0.1.0';
+    const message = 'HEALTH OK';
+
+    return { message, version };
   }
 }
