6ffbbbd34b51b6a3fd548c3d964d4fc3a91405d2
Hemin Johnson <hemin.johnson@diconium.com>
2023-04-16 23:08:43 +0200

    test: update test for lesson 2

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,10 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHealth()).toEqual({
+        message: 'HEALTH OK',
+        version: '0.1.0',
+      });
     });
   });
 });
